4110d4c4a1dba4066f79900b222a6ca2390bcd4a
FyodorovMS <spoon03@mail.ru>
2021-10-17 21:59:15 +0300

    Семинар 1. Задания 3

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-"""Семинар 1.Задания 1 и 2."""
+"""Семинар 1."""
 
 from collections import Generator
 
@@ -8,7 +8,7 @@ def my_gen_1(a: int, b: int) -> Generator:
     Семинар1.Задание 1.
 
     :param a: Начало интервала. 1 в задании
-    :param b: Конец интервала. 2 в задании
+    :param b: Конец интервала. 20 в задании
     :return: Возвращает квадрат либо куб числа.
     """
     r = range(a, b + 1)
@@ -35,5 +35,28 @@ def my_gen_fib_2(n: int) -> Generator:
         a, b = b, a + b
 
 
-for k in my_gen_fib_2(10):
-    print(k)
+def validation(text: str, *suffix: str) -> bool:
+    """
+    Семинар 1.Задание 2.
+
+    :param text: Строка для проверки.
+    :param suffix: Набор фуффиксов для поиска в строке.
+    :return: Если найден хоть один то true.
+    """
+    result = {}
+    separate = text.split('_')
+    for sf in suffix:
+        for pos in separate:
+            if pos == sf:
+                result[sf] = True
+                break
+            else:
+                result[sf] = False
+    for v in result.values():
+        if not v:
+            return False
+            break
+    return True
+
+
+print(validation('qwe_rty_ttt1_yyy', 'qwe', 'rty', 'ttt'))
